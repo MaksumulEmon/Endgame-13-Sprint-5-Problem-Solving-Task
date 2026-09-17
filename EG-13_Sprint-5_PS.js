8917ecc92ad7b9bdb5da2be7ec769e5c3c8b0ceb
MaksumulEmon <maksumulemon300@gmail.com>
2026-09-17 22:11:45 +0600

    initial commit- Binary Search

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -1,3 +1,5 @@
+// 01. Remove Duplicates from Sorted Array
+
 var removeDuplicates = function(nums) {
     let k = 1;
 
@@ -12,3 +14,22 @@ var removeDuplicates = function(nums) {
 
 let nums = [1, 1, 2];
 console.log(removeDuplicates(nums)); // 2
+
+
+// 02. Binary Search
+
+var search = function(nums, target) {
+    let left = 0, right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) return mid;
+        if (nums[mid] < target) left = mid + 1;
+        else right = mid - 1;
+    }
+
+    return -1;
+};
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9)); // 4
