7da4c4be3d83665e7d18e67bb8b88b01af6304d8
MaksumulEmon <maksumulemon300@gmail.com>
2026-09-17 22:04:40 +0600

    Remove Duplicates from Sorted Array

---
+++ EG-13_Sprint-5_PS.js
@@ -0,0 +1,14 @@
+var removeDuplicates = function(nums) {
+    let k = 1;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[k++] = nums[i];
+        }
+    }
+
+    return k;
+};
+
+let nums = [1, 1, 2];
+console.log(removeDuplicates(nums)); // 2
